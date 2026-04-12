21d26d5c95481c709403e3e43f0d27c2d627c209
edgewoodhsrobotics <edgewoodhighschoolrobotics@gmail.com>
2026-04-12 14:23:42 -0500

    Refactor outreachRenderer.js to streamline event rendering
    Removed commented-out code and redundant canvas retrieval.

--- public/javascript/outreachRenderer.js
+++ public/javascript/outreachRenderer.js
@@ -1,42 +1,28 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const canvas = document.getElementById('canvas');
+  if (!canvas) {
+    console.error('Element with id="canvas" not found in HTML.');
+    return;
+  }
 
-// This renders the outreach events for the team on the front page!
+  const events = [
+    { "name": "Edgewood Club Fair 2025", "image": "https://i.postimg.cc/qqk9X7w6/IMG-9474.jpg", "description": "Inspiring future team members to join robotics" },
+    { "name": "Advocacy Day 2026", "image": "https://i.postimg.cc/qqk9X7w6/IMG-9474.jpg", "description": "Informing lawmakers about STEM initiatives" },
+    { "name": "St. James School Presentation 2026", "image": "https://i.postimg.cc/qqk9X7w6/IMG-9474.jpg", "description": "Bringing robotics to K-8 schools" },
+    { "name": "Edgewood Campus School Presentation 2026", "image": "https://i.postimg.cc/qqk9X7w6/IMG-9474.jpg", "description": "Bringing robotics to K-8 schools" },
+    { "name": "Blessed Sacrament School Presentation 2026", "image": "https://i.postimg.cc/qqk9X7w6/IMG-9474.jpg", "description": "Bringing robotics to K-8 schools" }
+  ];
 
-const canvas = document.getElementById('canvas');
+  for (const event of events) {
+    let image = new ELMNT('img', event.image);
+    let title = new ELMNT('h1', event.name);
+    let desc = new ELMNT('p', event.description);
+    let card = document.createElement('div');
+    card.classList.add('outreachCard');
 
-/*
- * To add new outreach events copy and paste the below template into the below events array!
- *
- *  { "name": "EVENT NAME", "image": "https://i.postimg.cc/qqk9X7w6/IMG-9474.jpg", "description": "Desc" },
- * 
- * The last item CANNOT have a comma OR semicolon after it!
- */
-
-const events = [
-
-  { "name": "Edgewood Club Fair 2025", "image": "https://i.postimg.cc/qqk9X7w6/IMG-9474.jpg", "description": "Inspiring future team members to join robotics" },
-  { "name": "Advocacy Day 2026", "https://i.postimg.cc/qqk9X7w6/IMG-9474.jpg", "description": "Informing lawmakers about STEM initiatives" },
-  { "name": "St. James School Presentation 2026", "image": "https://i.postimg.cc/qqk9X7w6/IMG-9474.jpg", "description": "Bringing robotics to K-8 schools" },
-  { "name": "Edgewood Campus School Presentation 2026", "image": "https://i.postimg.cc/qqk9X7w6/IMG-9474.jpg", "description": "Bringing robotics to K-8 schools" },
-  { "name": "Blessed Sacrament School Presentation 2026", "image": "https://i.postimg.cc/qqk9X7w6/IMG-9474.jpg", "description": "Bringing robotics to K-8 schools" }
-  
-
-];
-
-for (const event of events) {
-
-  let image = new ELMNT('img', event.image);
-  let title = new ELMNT('h1', event.name);
-  let desc = new ELMNT('p', event.description);   
-  let card = document.createElement('div');
-
-  card.appendChild(image.el);
-  card.appendChild(title.el);
-  card.appendChild(desc.el);
-  card.classList.add('outreachCard');
-
-
-  canvas.appendChild(card);
-
-}
-
-
+    card.appendChild(image.el);
+    card.appendChild(title.el);
+    card.appendChild(desc.el);
+    canvas.appendChild(card);
+  }
+});   
